1235f7bccfd519ed641c51406ef4260784a02aae
Raymond Seger <raymondseger@live.com>
2018-12-23 11:58:22 +0700

    set push notification
    1. must be click / touch based when you want to ask for Notification permission, otherwise it's violation.
    
    2. must use promise for the push event listener because only browser can decide when to do the code you wanted for push events.

--- raymond-sw.js
+++ raymond-sw.js
@@ -4,6 +4,9 @@ var filesToCache    = [
   'http://localhost/test-pwa/test-PWA/app_data/apple.png'
 ];
 
+var the_sw = self;
+
+// get all the cached files
 self.addEventListener('install', function(event) {
     console.log('[ServiceWorker] Install');
     // Perform install steps
@@ -19,6 +22,7 @@ self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activate');
 });
 
+// every asset fetch will pass through Service Worker first
 self.addEventListener('fetch', function(e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
     e.respondWith(
@@ -26,4 +30,34 @@ self.addEventListener('fetch', function(e) {
         return response || fetch(e.request);
       })
     );
+});
+
+self.addEventListener('push', function(event) {
+    if (event.data) {
+      console.log('This push event has data: ', event.data.text());
+    } else {
+      console.log('This push event has no data.');
+    }
+
+    var promise1 = new Promise(function(resolve, reject) {
+        console.log('do promise 1');
+        resolve("done");
+      });
+
+    var promise2 = new Promise(function(resolve, reject) {
+        console.log('do promise 2');
+        const title     = "title";
+        const message   = "message";
+
+        self.registration.showNotification(title, {
+            body: message
+        });
+    });
+
+  const doThesePromises = Promise.all([
+    promise1,
+    promise2
+  ]);
+
+  event.waitUntil(doThesePromises);
 });
